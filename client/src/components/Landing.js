360125cf4d20d737c8987663e20e4db6aac276da
Ruijun Deng <ruijund@ruijuns-mbp.lan>
2018-01-08 22:42:41 -0800

    change icon to ESurvey

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Emaily!</h1>
-      Collect feedback from your users
+      <h1>ESurvey!</h1>
+      After login, Click on icon in the top right corner to explore!
     </div>
   );
 };
